975049375b74528936fd2d7ee63fdaf8c6dd23c5
alexjhoel <52894163+alexjhoel@users.noreply.github.com>
2022-09-09 20:14:22 -0300

    Retoques

--- Admin.Designer.cs
+++ Admin.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Admin));
             this.tablaDgv = new System.Windows.Forms.DataGridView();
             this.cedulaTbx = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -216,6 +217,7 @@
             this.Controls.Add(this.cedulaTbx);
             this.Controls.Add(this.tablaDgv);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Admin";
--- Login.Designer.cs
+++ Login.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.usuarioTbx = new System.Windows.Forms.TextBox();
             this.contraseniaTbx = new System.Windows.Forms.TextBox();
             this.ingresarBtn = new System.Windows.Forms.Button();
@@ -88,7 +89,7 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Iniciar sesión";
             // 
-            // Form1
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -99,7 +100,8 @@
             this.Controls.Add(this.ingresarBtn);
             this.Controls.Add(this.contraseniaTbx);
             this.Controls.Add(this.usuarioTbx);
-            this.Name = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
             this.Text = "Sorteo - Inicio de sesion";
             this.ResumeLayout(false);
             this.PerformLayout();
